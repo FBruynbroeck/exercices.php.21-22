18e1d732658fa8bc71d37262d78d09327a194c99
FBruynbroeck <francois.bruynbroeck@hotmail.com>
2022-01-13 09:51:19 +0100

    Ajout de exercice8

---
+++ exercice8/index.php
@@ -0,0 +1,23 @@
+<?php
+define('ROOT_PATH', "/exercice8/"); // Chemin qui suit le nom de domaine. Exemple: http://monprojet.local/exercice8/ le path a indiqué sera donc '/exercice8/'
+$request = str_replace(ROOT_PATH, "", $_SERVER['REQUEST_URI']); // On récupère juste la request, sans le chemin du dossier.
+$request = strtok($request, '?'); // Permet de supprimer les données envoyées en GET
+$request = trim($request, '/'); // Permet de supprimer le slash devant et à la fin de la request si elle existe
+$segments = array_filter(explode('/', $request)); // On découpe la requête pour obtenir une liste et on supprime les éléments Null
+if (!$segments){
+    $segments[0] = 'welcome'; // Si rien dans segments alors on injecte la page "welcome" qui sera la page par défaut (page d'accueil)
+}
+// Structure URL: http://monprojet.be/{REQ_TYPE}/{REQ_TYPE_ID}/{REQ_ACTION}
+define('REQ_TYPE', $segments[0] ?? Null);
+define('REQ_TYPE_ID', $segments[1] ?? Null);
+define('REQ_ACTION', $segments[2] ?? Null);
+$file = 'pages/'.REQ_TYPE.'.php';
+if(file_exists($file)){ // On vérifie que le fichier php existe
+    require $file;
+    exit();
+}
+else {
+    require 'pages/404.php';
+    exit();
+}
+?>
---
+++ exercice8/pages/welcome.php
@@ -0,0 +1,4 @@
+<?php
+$title = 'Exercice 8';
+$content = 'Welcome<br><a href="article">Article</a><br>';
+include('../template.php');
